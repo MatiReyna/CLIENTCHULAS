68a4d2485b1679a04b61e78e12ac2708f3cd2901
Matias Reyna <matiireyna@hotmail.com>
2024-04-26 13:57:56 -0300

    Mejoras en componente y estilos: NavBar

--- client/src/components/header/components/NavBar.jsx
+++ client/src/components/header/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Flex } from 'antd';
 import { useLocation, Link } from 'react-router-dom';
 
 import style from './NavBar.module.css';  //? Importamos los estilos del componente.
@@ -11,22 +10,20 @@ const NavBar = () => {
     return (
         <div>
             <nav>
-                <Flex gap='large' wrap='wrap'>
-                    <ul className={style.container}>
-                        <li className={pathname === '/' ? style.active : ''}>
-                            <Link to='/'>HOME</Link>
-                        </li>
-                        <li>
-                            <Link to='/calendar'>TURNERO</Link>
-                        </li>
-                        <li>
-                            <Link to='/about'>SOBRE MI</Link>
-                        </li>
-                    </ul>
-                </Flex>
+                <ul className={style.container}>
+                    <li className={pathname === '/' ? style.active : ''}>
+                        <Link to='/'>HOME</Link>
+                    </li>
+                    <li className={pathname === '/calendar' ? style.active : ''}>
+                        <Link to='/calendar'>TURNERO</Link>
+                    </li>
+                    <li className={pathname === '/about' ? style.active : ''}>
+                        <Link to='/about'>SOBRE MI</Link>
+                    </li>
+                </ul>
             </nav>
         </div>
     )
 };
 
-export default NavBar
+export default NavBar;
