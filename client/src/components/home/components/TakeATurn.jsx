b92c68def5456e03a9cc7e6e0d5f01aecafad0f6
Matias Reyna <matiireyna@hotmail.com>
2024-05-06 13:21:22 -0300

    Cambios en el component: TakeATurn

--- client/src/components/home/components/TakeATurn.jsx
+++ client/src/components/home/components/TakeATurn.jsx
@@ -3,48 +3,65 @@ import { CalendarOutlined, CarryOutOutlined, ClockCircleOutlined, LikeOutlined }
 import { Avatar } from 'antd';
 
 import style from './TakeATurn.module.css';  //? Importamos los estilos del componente.
+import buscar from '../utils/buscar.webp';
+import seleccionar from '../utils/seleccionar.webp';
+import esperar from '../utils/esperar.webp';
 
 const TakeATurn = () => {
 
     return (
-        <div className={style.container}>
-            <div className={style.title}>
-                ¡Sigue estos simples pasos para sacar un turno!
-            </div>
+        // <div className={style.container}>
+        //     <div className={style.title}>
+        //         ¡Sigue estos simples pasos para sacar un turno!
+        //     </div>
 
-            <div className={style.stepsContainer}>
-                <div className={style.step}>
-                    <Avatar
-                        className={style.icon}
-                        size={70}
-                        icon={ <CalendarOutlined /> }
-                    />
-                    <p>Busca el día y hora en el turnero</p>
-                </div>
-                <div className={style.step}>
-                    <Avatar
-                        className={style.icon}
-                        size={70}
-                        icon={ <CarryOutOutlined /> }
-                    />
-                    <p>Selecciona el que más te convenga</p>
-                </div>
-                <div className={style.step}>
-                    <Avatar
-                        className={style.icon}
-                        size={70}
-                        icon={ <ClockCircleOutlined /> }
-                    />
-                    <p>Aguarda que te confirmen el turno...</p>
-                </div>
-                <div className={style.step}>
-                    <Avatar
-                        className={style.icon}
-                        size={70}
-                        icon={ <LikeOutlined /> }
-                    />
-                    <p>¡Listo, disfruta de tus nuevas uñas!</p>
-                </div>
+        //     <div className={style.stepsContainer}>
+        //         <div className={style.step}>
+        //             <Avatar
+        //                 className={style.icon}
+        //                 size={70}
+        //                 icon={ <CalendarOutlined /> }
+        //             />
+        //             <p>Busca el día y hora en el turnero</p>
+        //         </div>
+        //         <div className={style.step}>
+        //             <Avatar
+        //                 className={style.icon}
+        //                 size={70}
+        //                 icon={ <CarryOutOutlined /> }
+        //             />
+        //             <p>Selecciona el que más te convenga</p>
+        //         </div>
+        //         <div className={style.step}>
+        //             <Avatar
+        //                 className={style.icon}
+        //                 size={70}
+        //                 icon={ <ClockCircleOutlined /> }
+        //             />
+        //             <p>Aguarda que te confirmen el turno...</p>
+        //         </div>
+        //         <div className={style.step}>
+        //             <Avatar
+        //                 className={style.icon}
+        //                 size={70}
+        //                 icon={ <LikeOutlined /> }
+        //             />
+        //             <p>¡Listo, disfruta de tus nuevas uñas!</p>
+        //         </div>
+        //     </div>
+        // </div>
+        <div className={style.takeaturnContainer}>
+            <div className={style.seleccion}>
+                <img src={buscar} alt='buscar' />
+            </div>
+            <div className={style.seleccion}>
+                <img src={seleccionar} alt='buscar' />
+            </div>
+            <div className={style.seleccion}>
+                <img src={esperar} alt='buscar' />
+            </div>
+            <div className={style.seleccion}>
+                <img src='./about-moro.webp' alt='buscar' />
             </div>
         </div>
     )
