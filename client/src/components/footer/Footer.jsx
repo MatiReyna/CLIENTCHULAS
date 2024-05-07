2183a25184da8de45d82ee5253ba5ba085483e68
Matias Reyna <matiireyna@hotmail.com>
2024-05-07 18:02:59 -0300

    Nuevo Footer: in process

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -10,80 +10,93 @@ import style from './Footer.module.css';  //? Importamos los estilos del compone
 const Footer = () => {
 
     return (
+        // <div className={style.footerContainer}>
+        //     <div>
+        //         <img src='/logo-pagina.webp' alt='logoFooter' className={style.logoFooter} />
+        //     </div>
+        //     <div>
+        //         <Link to={'/faq'}>
+        //             <p>{FAQ}</p>
+        //         </Link>
+        //         <Link to={'/change-policies'}>
+        //             <p>{changePolicies}</p>
+        //         </Link>
+        //     </div>
+        //     <div>
+        //         <p>{contact}</p>
+        //         <p style={{ fontWeight: 'bold' }}>{callPhone}</p>
+        //         <p style={{ fontWeight: 'bold' }}>{email}</p>
+        //         <p>{dayOperation}</p>
+        //         <p>{day}</p>
+        //         <p>¡Gracias por visitarnos!</p>
+        //     </div>
+
+        //     <a
+        //         href='https://www.instagram.com/misschulas/?igsh=MXRzOTFmMm9qNXhyeQ%3D%3D'
+        //         target='_blank'
+        //         rel='noopener noreferrer'
+        //     >
+        //         <Avatar
+        //             className={style.icon}
+        //             size={70}
+        //             icon={ <InstagramOutlined /> }
+        //         />
+        //     </a>
+        //     <p>Seguinos para enterarte de todo</p>
+
+        //     <div className={style.footerCopyright}>
+        //         <p>@2024 Miss Chulas, Inc. All rights reserved.</p>
+        //         <p>Privacy Policy.</p>
+        //         <p>Terms of Use.</p>
+        //     </div>
+        // </div>
         <div className={style.footerContainer}>
-            <div>
-                <img src='/logo-pagina.webp' alt='logoFooter' className={style.logoFooter} />
-            </div>
-            <div>
-                <Link to={'/faq'}>
-                    <p>{FAQ}</p>
-                </Link>
-                <Link to={'/change-policies'}>
-                    <p>{changePolicies}</p>
-                </Link>
-            </div>
-            <div>
-                <p>{contact}</p>
-                <p style={{ fontWeight: 'bold' }}>{callPhone}</p>
-                <p style={{ fontWeight: 'bold' }}>{email}</p>
-                <p>{dayOperation}</p>
-                <p>{day}</p>
-                <p>¡Gracias por visitarnos!</p>
-            </div>
+            <div className={style.footer}>
+                <div className={style.newslatter}>
+                    <div className={style.newslatterLeft}>
+                        <h2>Sbscribe to our</h2>
+                        <h1>Newslatter</h1>
+                    </div>
+                    <div className={style.newslatterRight}>
+                        <div className={style.newslatterInput}>
+                            <input type='text' placeholder='Enter your email adress' />
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
 
-            <a
-                href='https://www.instagram.com/misschulas/?igsh=MXRzOTFmMm9qNXhyeQ%3D%3D'
-                target='_blank'
-                rel='noopener noreferrer'
-            >
-                <Avatar
-                    className={style.icon}
-                    size={70}
-                    icon={ <InstagramOutlined /> }
-                />
-            </a>
-            <p>Seguinos para enterarte de todo</p>
-
-            <div className={style.footerCopyright}>
-                <p>@2024 Miss Chulas, Inc. All rights reserved.</p>
-                <p>Privacy Policy.</p>
-                <p>Terms of Use.</p>
+                <div>
+                    <div>
+                        <h2>Vee Teach</h2>
+                        <p>Distinctio, architecto perferendis corrupti deleniti, cum assumenda, accusantium neque illum nobis ipsa necessitatibus dolores itaque?.</p>
+                        <div>
+                            <a herf='#'><i></i></a>
+                            <a herf='#'><i></i></a>
+                            <a herf='#'><i></i></a>
+                            <a herf='#'><i></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p>Information</p>
+                    <a>Our Company</a>
+                    <a>About Us</a>
+                    <a>Blog</a>
+                </div>
+                <div>
+                    <p>Helpful Links</p>
+                    <a>Service</a>
+                    <a>Support</a>
+                    <a>Terms & Condition</a>
+                </div>
+                <div>
+                    <p>Navegation</p>
+                    <a>Home</a>
+                    <a>About</a>
+                    <a>Contact</a>
+                </div>
             </div>
         </div>
-        // <footer className={style.footerContainer}>
-        //     <div className={style.footerWrapper}>
-        //         <div className={style.footerSection}>
-        //             <div className={style.footerSubscribe}>
-        //                 <h2>
-        //                     Subscribe to the Miss Chulas® newslatter to say
-        //                     up-to-date with the latest releases.
-        //                 </h2>
-        //                 <div className={style.box}>
-        //                     <input type='email' placeholder='Email Address' />
-        //                     <button>💖</button>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //         <nav className={style.footerNav}>
-        //             <div>
-        //                 <h3>Company</h3>
-        //                 <ul>
-        //                     <li><a>About</a></li>
-        //                     <li><a>Blog</a></li>
-        //                     <li><a>Contact Us</a></li>
-        //                 </ul>
-        //             </div>
-        //             <div>
-        //                 <h3>Connect</h3>
-        //             </div>
-        //         </nav>
-        //         <div className={style.footerCopyright}>
-        //             <p>©2024 Miss Chulas, Inc. All rights reserved.</p>
-        //             <p>Privacy Policy.</p>
-        //             <p>Terms of Use.</p>
-        //         </div>
-        //     </div>
-        // </footer>
     )
 };
 
