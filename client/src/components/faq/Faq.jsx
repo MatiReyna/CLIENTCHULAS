6ba230483082ad55151d68e5e0439211f1548f73
Matias Reyna <matiireyna@hotmail.com>
2024-05-15 12:35:12 -0300

    Component y estilos: Faq

--- client/src/components/faq/Faq.jsx
+++ client/src/components/faq/Faq.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
-import { Collapse } from "antd";
+import { ClockCircleOutlined } from '@ant-design/icons';
+import { Timeline } from "antd";
+
+import style from './Faq.module.css';  //? Importamos los estilos del componente.
 
 const Faq = () => {
     
     return (
-        <div>
-            <Collapse />
+        <div className={style.faqContainer}>
+            <Timeline
+                mode='alternate'
+                items={[
+                    {
+                        children: 'Create a services site 2015-09-01'
+                    },
+                    {
+                        children: 'Solve initial network problems 2015-09-01',
+                        color: 'green'
+                    },
+                    {
+                        dot: (
+                            <ClockCircleOutlined
+                                className={style.timelineItemDot}
+                                style={{ fontSize: '20px' }}
+                            />
+                        ),
+                        children: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.'
+                    },
+                    {
+                        color: 'red',
+                        children: 'Network problems being solved 2015-09-01'
+                    },
+                ]}
+            />
         </div>
     )
 };
